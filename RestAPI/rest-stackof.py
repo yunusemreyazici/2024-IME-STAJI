30e5a92aae7e351e92fda6b9b1a282e6df67f994
Yunus Emre YAZICI <yunusemredevyazici@gmail.com>
2024-03-14 13:11:54 +0300

    Update rest-stackof.py

--- RestAPI/rest-stackof.py
+++ RestAPI/rest-stackof.py
@@ -2,6 +2,12 @@ import requests
 import json
 
 
-response= requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
+response= requests.get(
+    'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow'
+    )
 
-print(response.json()['items'])
+for data in response.json()['items']:
+    print(data['title'])
+    print(data['link'])
+    print()
+
